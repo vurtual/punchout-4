02d191518b0b62856970a926484bed8cceb231b9
vurtual <vurtual@users.noreply.github.com>
2023-06-05 15:04:20 +0100

    Router operational

--- src/server/serve.js
+++ src/server/serve.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { router } = require('./routes');
 const app = express();
 
 require('dotenv').config();
@@ -6,6 +7,8 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const PROTOCOL = process.env.PROTOCOL;
 const URL = process.env.URL;
+
+app.use('/', router);
 
 const launchServer = app.listen(PORT, () =>
   console.log(`Server running at ${PROTOCOL}://${URL}:${PORT}`)
